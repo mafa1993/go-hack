426dfd33f9c54ce7341cce296451b24458d88797
mafa1993 <378909613@qq.com>
2022-08-31 18:58:32 +0800

    shodan查询小工具完成

--- shodan/main.go
+++ shodan/main.go
@@ -1,8 +1,39 @@
 package main
 
-import "shodan/shodan"
+import (
+	"fmt"
+	"log"
+	"os"
+	"shodan/shodan"
+)
 
 func main() {
-	client := shodan.New("V3RD7hhexN80bnD7He7pkz7UKjeoRewh")
-	client.APIInfo()
+
+	//client.APIInfo()
+	//client.HostSearch("thinkphp")
+	if len(os.Args) != 2 {
+		log.Fatalln("参数不对")
+	}
+
+	//shodan的api放到环境变量里
+	apiKey := os.Getenv("SHODAN_API_KEY")
+
+	s := shodan.New(apiKey)
+
+	info, err := s.APIInfo()
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Printf("query credits:%d,\nnScan credits:%d\n", info.QueryCredits, info.ScanCredits)
+
+	HostSearch, err := s.HostSearch(os.Args[1])
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	for _, host := range HostSearch.Matches {
+		fmt.Printf("%18s,%d\n", host.IPString, host.Port)
+	}
 }
--- shodan/shodan/host.go
+++ shodan/shodan/host.go
@@ -1,3 +1,74 @@
 package shodan
 
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
 // 查询具体内容的函数
+
+// https://api.shodan.io/shodan/host/search?key=?&query=?&facets=?
+
+// location 数据
+type HostLocation struct {
+	City         string  `json:"city"`
+	RegionCode   string  `json:"region_code"`
+	AreaCode     int     `json:"area_code"`
+	Longitude    float32 `json:"longitude"`
+	CountryCode3 string  `json:"country_code3"`
+	CountryName  string  `json:"country_name"`
+	PostalCode   int     `json:"postal_code"`
+	DMACode      string  `json:"dma_code"`
+	Latitude     float32 `json:"latitude"`
+}
+
+// matches 每一个元素
+type Host struct {
+	OS        string       `json:"os"`
+	Location  HostLocation `json:"location"`
+	Timestamp string       `json:"timestamp"`
+	ISP       string       `json:"isp"`
+	ASN       string       `json:"asn"`
+	Hostname  []string     `json:"hostname"`
+	IP        int64        `json:"ip"`
+	Domain    []string     `json:"domain"`
+	Org       string       `json:"org"`
+	Data      string       `json:"data"`
+	Port      int          `json:"port"`
+	IPString  string       `json:"ip_str"`
+}
+
+// 解析matches数组
+type HostSearch struct {
+	Matches []Host `json:"matches"`
+}
+
+// @params q 查询的内容
+// 此函数用于查询
+func (client *Client) HostSearch(q string) (*HostSearch, error) {
+	url := fmt.Sprintf("%s/shodan/host/search?key=%s&query=%s", BASEURL, client.apiKey, q)
+
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		fmt.Println(err)
+		return &HostSearch{}, err
+	}
+	var c http.Client
+	resp, err := c.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return &HostSearch{}, err
+	}
+
+	defer resp.Body.Close()
+
+	var rtn HostSearch
+	if err := json.NewDecoder(resp.Body).Decode(&rtn); err != nil {
+		fmt.Println(err)
+		return &HostSearch{}, err
+	}
+	fmt.Printf("%+v", rtn)
+
+	return &rtn, nil
+}
