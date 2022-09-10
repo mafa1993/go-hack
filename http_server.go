3835cc01dd3ca02170738843a292426bd649eb31
mafa1993 <378909613@qq.com>
2022-09-10 16:56:55 +0800

    中间件简单实现

--- http_server.go
+++ http_server.go
@@ -10,6 +10,10 @@ import (
 
 type route struct{}
 
+type logger struct {
+	Inner http.Handler
+}
+
 // 构建路由函数
 func (r route) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	switch req.URL.Path {
@@ -23,10 +27,18 @@ func (r route) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 
 }
 
+// 两个serveHttp，先执行这个，这里再调用真正的servehttp
+func (l logger) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	fmt.Println(11)
+	l.Inner.ServeHTTP(w, req)
+	fmt.Println(22)
+}
+
 func main() {
 	var r route
 	//http.HandleFunc("/hello", hello) // 这也为路由实现，如果handleFunc和http.Handler一起存在，前者不生效
-	log.Fatalln(http.ListenAndServe(":8000", &r))
+	l := logger{Inner: r}
+	log.Fatalln(http.ListenAndServe(":8000", &l))
 }
 
 func hello(w http.ResponseWriter, r *http.Request) {
