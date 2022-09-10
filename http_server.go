4a376ab336d1cef743bb4bf119e739a10202386d
mafa1993 <378909613@qq.com>
2022-09-10 15:25:35 +0800

    增加注释

--- http_server.go
+++ http_server.go
@@ -11,19 +11,24 @@ import (
 type route struct{}
 
 // 构建路由函数
-func (r route)ServeHTTP(w http.ResponseWriter,req *http.Request){
+func (r route) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	switch req.URL.Path {
 	case "/a":
-		fmt.Fprint(w,"is a")
+		fmt.Fprint(w, "is a")
 	case "/b":
-		fmt.Fprintf(w,"is b")
+		fmt.Fprintf(w, "is b")
 	default:
-		http.Error(w,"not found",http.StatusNotFound)
+		http.Error(w, "not found", http.StatusNotFound)
 	}
 
 }
 
-func main () {
+func main() {
 	var r route
-	log.Fatalln(http.ListenAndServe(":8000",&r))
+	//http.HandleFunc("/hello", hello) // 这也为路由实现，如果handleFunc和http.Handler一起存在，前者不生效
+	log.Fatalln(http.ListenAndServe(":8000", &r))
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "hello")
 }
